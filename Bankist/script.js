3e15f265aa73e85861af1af4b1a0a3bc80b30637
prasad99999 <chaudhariprasad69@gmail.com>
2021-10-09 11:12:14 +0530

    created displayMovements fun.

--- Bankist/script.js
+++ Bankist/script.js
@@ -60,13 +60,42 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+const displayMovements = function(movements){
+   containerMovements.innerHTML = ''; // reset to empty before adding new movements__row.
+
+   movements.forEach(function(mov,i){
+
+       //creating  div element with class movements__row in each iteration. and change some class OR data according
+       // mov val.
+       const type = mov > 0 ? 'deposit' : 'withdrawal';        
+
+       const html = `
+       <div class="movements__row">                         
+            <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+            <div class="movements__value">4 000€</div>
+       </div>
+       `;
+       // adding html into given element.
+       containerMovements.insertAdjacentHTML('afterbegin',html); 
+   });
+}
+
+displayMovements(account1.movements);
+
+
+
+
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
