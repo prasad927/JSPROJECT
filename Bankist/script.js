485e8b0f618a4415cfab37491a239dacb2fe66d2
prasad99999 <chaudhariprasad69@gmail.com>
2021-10-10 16:39:24 +0530

    push

--- Bankist/script.js
+++ Bankist/script.js
@@ -191,6 +191,21 @@ btnTransfer.addEventListener('click',function(e){
   }
 });
 
+//close account.
+btnClose.addEventListener('click',function(e){
+  e.preventDefault();
+
+  if(inputCloseUsername.value===currentAccount.username && Number(inputClosePin.value)===currentAccount.pin){
+      //valid usename && userpin
+      const index = accounts.findIndex(acc=> acc.username===currentAccount.username);
+      //delete account
+      accounts.splice(index,1);
+      //HideUI
+      containerApp.style.opacity=0;
+  }
+  inputCloseUsername.value = inputClosePin.value='';
+
+});
 
 
 
